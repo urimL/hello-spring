770935d945f85c49563c206b8af152bca4c87937
toulim <toulim0525@ewhain.net>
2023-08-14 13:15:18 +0900

    회원 domain,repository

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.swing.text.html.Option;
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long,Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(long id) {
+        //null이 반환될 가능성이 있으므로 Optional에 감싸서 반환
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //stream 내부 반복자 이용하여 요소들을 반복시킨다.
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findALl() {
+        return new ArrayList<>(store.values());
+    }
+}
